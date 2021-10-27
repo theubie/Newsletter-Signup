10d89b700f221a65e20c6bf4b8f1d6eaa1319b0f
theubie <randell@nvcreativetechnology.com>
2021-10-27 10:06:52 -0500

    Added basic validation via express-validator Normaized email input whitelisted first and last names to a-zA-Z only (will need some work to support other characters)

--- apps.js
+++ apps.js
@@ -1,10 +1,14 @@
 //User Configuration
 const listID = "YOUR_LIST_ID";
 const apiKey = "YOUR_API_KEY";
+
 const port = 3000;
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const {
+  body
+} = require('express-validator');
 const https = require('https');
 
 const app = express();
@@ -18,49 +22,57 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res) {
-  const firstName = req.body.fname;
-  const lastName = req.body.lname;
-  const email = req.body.email;
+app.post("/",
+  body('email').normalizeEmail(),
+  body('fname').whitelist("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+  body('lname').whitelist("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+  function(req, res) {
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
 
-  const data = {
-    members: [{
-      email_address: email,
-      status: "subscribed",
-      merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName
-      }
-    }]
-  }
-
-  const jsonData = JSON.stringify(data);
-
-  const dataCenter = apiKey.split('-')[1]; //Grab this from the end of the API Key
-  const url = "https://" + dataCenter + ".api.mailchimp.com/3.0/lists/" + listID;
-  const options = {
-    method: "POST",
-    auth: "anystring:" + apiKey,
-
-  };
-  const request = https.request(url, options, function(response) {
-    if(response.statusCode == 200) {
-      //Success
-      res.sendFile(__dirname + "/success.html");
-    } else {
-      //Fail
-      res.sendFile(__dirname + "/failure.html");
+    const data = {
+      members: [{
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }]
     }
-    response.on("data", function(data) {
-      //console.log(JSON.parse(data));
-    })
+
+    const jsonData = JSON.stringify(data);
+
+    const dataCenter = apiKey.split('-')[1]; //Grab this from the end of the API Key
+    const url = "https://" + dataCenter + ".api.mailchimp.com/3.0/lists/" + listID;
+    const options = {
+      method: "POST",
+      auth: "anystring:" + apiKey,
+
+    };
+    const request = https.request(url, options, function(response) {
+      if (response.statusCode == 200) {
+        //Success
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        //Fail
+        res.sendFile(__dirname + "/failure.html");
+      }
+      response.on("data", function(data) {
+        //console.log(JSON.parse(data));
+      })
+    });
+    request.on('error', (e) => {
+      //Trap any errors reaching the server
+      console.error(e);
+      res.sendFile(__dirname + "/failure.html");
+    });
+    request.write(jsonData); //Send the data
+    request.end(); // End the request
   });
 
-  request.write(jsonData); //Send the data
-  request.end(); // End the request
-});
-
-app.post("/failure",function(req,res){
+app.post("/failure", function(req, res) {
   res.redirect("/");
 });
 
